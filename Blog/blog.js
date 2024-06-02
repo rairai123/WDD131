f34ba974f94754283ab8d3707e4cdd486fcea1cb
Laerry Mahonri <laerry1258@gmail.com>
2024-06-01 23:50:52 -0600

    middle

--- Blog/blog.js
+++ Blog/blog.js
@@ -9,7 +9,7 @@ const articles = [
     imgAlt: "Book cover for Septimus Heap 1",
     ages: "10-14",
     genre: "Fantasy",
-    stars: "****",
+    stars: '<span class="rating">&#9733;&#9733;&#9733;&#9733;</span>',
   },
   {
     id: 2,
@@ -22,11 +22,23 @@ const articles = [
     imgAlt: "Book cover for Magnus Chase 1",
     ages: "12-16",
     genre: "Fantasy",
-    stars: "⭐⭐⭐⭐",
+    stars: '<span class="rating">&#9733;&#9733;&#9733;&#9733;&#9733;</span>',
+  },
+  {
+    id: 3,
+    title: "Belgariad Book One: Pawn of Prophecy",
+    date: "Feb 12, 2022",
+    description:
+      'A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his "Aunt Pol" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.',
+    imgSrc: "https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+    imgAlt: "Book cover for Pawn of Prophecy",
+    ages: "12-16",
+    genre: "Fantasy",
+    stars: '<span class="rating">&#9733;&#9733;&#9733;&#9733;&#9733;</span>',
   },
 ];
 
-const bookReviewsContainer = document.querySelector(".book-reviews");
+const bookReviewsContainer = document.querySelector(".book-reviews-container");
 
 articles.forEach((article) => {
   const bookReview = document.createElement("div");
@@ -35,7 +47,11 @@ articles.forEach((article) => {
   const bookDetails = document.createElement("div");
   bookDetails.classList.add("book-details");
 
+  const title = document.createElement("h2");
+  title.textContent = article.title;
+
   const date = document.createElement("p");
+  date.classList.add("date");
   date.textContent = article.date;
 
   const age = document.createElement("p");
@@ -44,9 +60,10 @@ articles.forEach((article) => {
   const genre = document.createElement("p");
   genre.textContent = article.genre;
 
-  const rating = document.createElement("p");
+  const rating = document.createElement("div");
   rating.innerHTML = article.stars;
 
+  bookDetails.appendChild(title);
   bookDetails.appendChild(date);
   bookDetails.appendChild(age);
   bookDetails.appendChild(genre);
@@ -55,9 +72,6 @@ articles.forEach((article) => {
   const bookContent = document.createElement("div");
   bookContent.classList.add("book-content");
 
-  const title = document.createElement("h2");
-  title.textContent = article.title;
-
   const image = document.createElement("img");
   image.src = article.imgSrc;
   image.alt = article.imgAlt;
@@ -65,7 +79,6 @@ articles.forEach((article) => {
   const description = document.createElement("p");
   description.textContent = article.description;
 
-  bookContent.appendChild(title);
   bookContent.appendChild(image);
   bookContent.appendChild(description);
 
